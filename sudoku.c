cda8238febd1479030141b236198294df2ac07d5
Replit user <>
2023-10-05 03:39:46 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -50,7 +50,7 @@ int is_valid(Node* n)
   
   for(int i=0 ; i<9 ; i++)
     {
-      for(int j=0; j<0 ; j++)
+      for(int j=0; j<9 ; j++)
         {
           int num_fila = n->sudo[i][j];
           int num_columnas = n->sudo[j][i];
